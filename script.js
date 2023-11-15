72693889aaa61dd2ec6f52545134f743afed8f6f
Moises Cardenas Ramirez <moshe@Air-de-Moises.home>
2023-11-15 19:15:54 +0100

    nuevos ficheros

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const saludo= ´hola mundo´
+
+const hora = new Date(). getHours();
+let mensaje;
+
+if (hora <13) mensaje = ´buenos dias´;
+else if (hora <21) mensaje = ´buenas tardes´;
+else if (hora <24) mensaje = ´buenas noches´;
