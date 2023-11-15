b195fc7603f5078b5e36686daafb2af152d93956
Moises Cardenas Ramirez <moshe@Air-de-Moises.home>
2023-11-15 19:29:42 +0100

    barra de menu

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const saludo= ´hola mundo´
-
-const hora = new Date(). getHours();
-let mensaje;
-
-if (hora <13) mensaje = ´buenos dias´;
-else if (hora <21) mensaje = ´buenas tardes´;
-else if (hora <24) mensaje = ´buenas noches´;
